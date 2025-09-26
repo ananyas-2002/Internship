e49151ab8c136b8c9ea46c2c16912dc42e39b1df
ananyas-2002 <149650933+ananyas-2002@users.noreply.github.com>
2025-09-26 13:02:27 +0530

    Add files via upload

---
+++ UserPanel/next.config.js
@@ -0,0 +1,8 @@
+module.exports =   {
+    swcMinify: true,
+    publicRuntimeConfig:{
+        APP_NAME: "MYHEALERDOC",
+        API_DEVELOPMENT:'http://localhost:3243/api',
+        PRODUCTION:false
+    }
+};
